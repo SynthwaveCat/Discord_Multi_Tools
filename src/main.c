eb82e57cbe52bdbf964a24c338004be3f426d2a1
SANAGOdev <sanago3130@gmail.com>
2023-01-01 01:06:44 +0100

    Update: GUI cleaning, Button add, Hover button, InputBox about to work

--- src/main.c
+++ src/main.c
@@ -6,12 +6,25 @@ int main(int argc, char const **argv)
 {
     //Initialisation des variables et structures CSFML
     sfRenderWindow *window;
-    sfRectangleShape *button;
-    sfVector2f titre_position = {300, 0};
-    sfVector2i window_vector = {200, 200};
-    sfText *Titre;
-    sfFont *font;
     sfEvent event;
+
+    // Initialisation des couleurs
+    sfColor sfDarkGreen = sfColor_fromRGB(44, 119, 9);
+
+    // Declarations des rectangles
+    sfRectangleShape *button;
+    sfRectangleShape *input_box;
+
+    // Declaration de text / fonts
+    sfText *Title;
+    sfText *submit_button;
+    sfFont *font;
+
+    // Declarations des vecteurs de positions
+    sfVector2f Title_position = {300, 0};
+    sfVector2f submit_button_position = {510, 100};
+    sfVector2i window_vector = {200, 200};
+    
 
     // Initialisation de la fenetre
     window = create_renderwindow(800, 600, 32, "GeoLocationIPTool");
@@ -24,12 +37,15 @@ int main(int argc, char const **argv)
     if (!font)
         return 84;
 
-    // Initialisation du titre
-    Titre = create_text("Geo IP Tool", font, sfBlack, 25);
-    sfText_setPosition(Titre, titre_position);
+    // Initialisation des textes
+    Title = create_text("Geo IP Tool", font, sfBlack, 25);
+    sfText_setPosition(Title, Title_position);
+    submit_button = create_text("Submit", font, sfBlack, 25);
+    sfText_setPosition(submit_button, submit_button_position);
 
     // Creation d'un paterne de rectangle
-    button = create_rectangle_shape(100, 50, 50, 50, sfGreen, sfBlack, 2);
+    button = create_rectangle_shape(100, 50, 510, 100, sfGreen, sfBlack, 2);
+    input_box = create_rectangle_shape(400, 50, 100, 100, sfTransparent, sfBlack, 2);
 
     sfRenderWindow_drawRectangleShape(window, button, NULL);
 
@@ -41,37 +57,45 @@ int main(int argc, char const **argv)
                 sfRenderWindow_close(window);
             }
 
-        // Bouton
-        if (mouse_on_rectangle(button, window))
-        {
-            if (click_on_rectangle(button, window))
-            {
-                printf("test");
-            }
-        }
-        
-        // Get text input
-            if (event.type == sfEvtTextEntered) {
-                if (event.text.unicode < 128) {
-                    printf("Ascii charactere typed : %c\n", (char)event.text.unicode);
+            // Bouton
+            if (mouse_on_rectangle(input_box, window)) {
+
+                if (click_on_rectangle(input_box, window)) {
+
+                    // Get text input
+                    if (event.type == sfEvtTextEntered) {
+                        if (event.text.unicode < 128) {
+                            printf("Ascii charactere typed : %c\n", (char)event.text.unicode);
+                        }
+                    }
                 }
             }
+            if (mouse_on_rectangle(button, window)) {
+                sfRectangleShape_setFillColor(button, sfDarkGreen);
+            }
+
+            else if (!mouse_on_rectangle(button, window)) {
+                sfRectangleShape_setFillColor(button, sfGreen);
+            }
+
+            // Couleurs de fond de la fenetre
+            sfRenderWindow_clear(window, sfWhite);
+
+            // Affichage des rectangles
+            sfRenderWindow_drawRectangleShape(window, button, NULL);
+            sfRenderWindow_drawRectangleShape(window, input_box, NULL);
+
+            // Affichage des textes
+            sfRenderWindow_drawText(window, Title, NULL);
+            sfRenderWindow_drawText(window, submit_button, NULL);
+
+            // Affichage de la fenetre
+            sfRenderWindow_display(window);
         }
-        // Couleurs de fond de la fenetre
-        sfRenderWindow_clear(window, sfWhite);
-
-        // Affichage du Titre
-        sfRenderWindow_drawText(window, Titre, NULL);
-
-        // Afficher le rectangle
-        sfRenderWindow_drawRectangleShape(window, button, NULL);
-
-        // Affichage de la fenetre
-        sfRenderWindow_display(window);
     }
-
     // Free des elements
-    sfText_destroy(Titre);
+    sfText_destroy(Title);
     sfRenderWindow_destroy(window);
     return 0;
+    
 }
--- src/mouse_on_rectangle.c
+++ src/mouse_on_rectangle.c
@@ -7,8 +7,7 @@ sfBool mouse_on_rectangle(sfRectangleShape *rectangle, sfRenderWindow *window)
     sfFloatRect rect = sfRectangleShape_getGlobalBounds(rectangle);
     sfVector2i mouse = sfMouse_getPositionRenderWindow(window);
 
-    if (mouse.x > rect.left && mouse.x < rect.left + rect.width
-        && mouse.y > rect.top && mouse.y < rect.top + rect.height)
+    if (mouse.x > rect.left && mouse.x < rect.left + rect.width && mouse.y > rect.top && mouse.y < rect.top + rect.height)
         return (sfTrue);
     return (sfFalse);
 }

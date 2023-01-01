28d8467f852eb86de64b2fc5025569e21dcea9fe
Laura <bartekchaton@gmail.com>
2023-01-01 15:14:36 +0100

    Update: macro and colors

--- include/my.h
+++ include/my.h
@@ -10,6 +10,43 @@
     #include <curl/curl.h>
     #include <json-c/json.h>
 
+    /* MACRO */
+
+    /* TITLE PROPERTY'S */
+    #define TITLE_POS_X                     300
+    #define TITLE_POS_Y                     0
+    #define TITLE_TEXT                      "Geo IP Tool"
+
+    /* WINDOW PROPERTY'S */
+
+    #define WINDOW_VECTOR_POS_X             200
+    #define WINDOW_VECTOR_POS_Y             200
+    #define WINDOW_SIZE_POS_X               800
+    #define WINDOW_SIZE_POS_Y               600
+    #define WINDOW_TEXT                     "GeoLocationIPTool"
+
+    /* SUBMIT BUTTON PROPERTY'S */
+
+    #define SUBMIT_BUTTON_SIZE_POS_X        100
+    #define SUBMIT_BUTTON_SIZE_POS_Y        50
+    #define SUBMIT_BUTTON_POS_X             510
+    #define SUBMIT_BUTTON_POS_Y             100
+    #define SUBMIT_BUTTON_TEXT              "Submit"
+
+    /* INPUT BOX PROPERTY'S */
+
+    #define INPUT_BOX_SIZE_POS_X            400
+    #define INPUT_BOX_SIZE_POS_Y            50
+    #define INPUT_BOX_POS_X                 100
+    #define INPUT_BOX_POS_Y                 100
+
+    /* COLOR */
+
+    #define sfDarkGreen sfColor_fromRGB(44, 119, 9)
+    #define sfGrey      sfColor_fromRGB(136, 136, 136)
+
+    /* FUNCTIONS */
+
     sfRenderWindow *create_renderwindow(unsigned int x, unsigned int y, unsigned int bpp, char *title);
     sfText *create_text(char *string, sfFont *font, sfColor text_color, unsigned int text_size);
     sfRectangleShape *create_rectangle_shape(float x_vector_size, float y_vector_size, float x_vector_position, float y_vector_position, sfColor fill_color, sfColor outline_color, float outline_thickness);
--- src/main.c
+++ src/main.c
@@ -8,10 +8,6 @@ int main(int argc, char const **argv)
     sfRenderWindow *window;
     sfEvent event;
 
-    // Initialisation des couleurs
-    sfColor sfDarkGreen = sfColor_fromRGB(44, 119, 9);
-    sfColor sfGrey = sfColor_fromRGB(136, 136, 136);
-
     // Initialisation des variables sfBool
     sfBool is_focused = sfFalse;
 
@@ -25,13 +21,12 @@ int main(int argc, char const **argv)
     sfFont *font;
 
     // Declarations des vecteurs de positions
-    sfVector2f Title_position = {300, 0};
-    sfVector2f submit_button_position = {510, 100};
-    sfVector2i window_vector = {200, 200};
+    sfVector2f Title_position = {TITLE_POS_X, TITLE_POS_Y};
+    sfVector2f submit_button_position = {SUBMIT_BUTTON_POS_X, SUBMIT_BUTTON_POS_Y};
+    sfVector2i window_vector = {WINDOW_VECTOR_POS_X, WINDOW_VECTOR_POS_Y};
     
-
     // Initialisation de la fenetre
-    window = create_renderwindow(800, 600, 32, "GeoLocationIPTool");
+    window = create_renderwindow(WINDOW_SIZE_POS_X, WINDOW_SIZE_POS_Y, 32, WINDOW_TEXT);
     if (!window)
         return 84;
     sfRenderWindow_setPosition(window, window_vector);
@@ -42,14 +37,14 @@ int main(int argc, char const **argv)
         return 84;
 
     // Initialisation des textes
-    Title = create_text("Geo IP Tool", font, sfBlack, 25);
+    Title = create_text(TITLE_TEXT, font, sfBlack, 25);
     sfText_setPosition(Title, Title_position);
-    submit_button = create_text("Submit", font, sfBlack, 25);
+    submit_button = create_text(SUBMIT_BUTTON_TEXT, font, sfBlack, 25);
     sfText_setPosition(submit_button, submit_button_position);
 
     // Creation d'un paterne de rectangle
-    button = create_rectangle_shape(100, 50, 510, 100, sfGreen, sfBlack, 2);
-    input_box = create_rectangle_shape(400, 50, 100, 100, sfTransparent, sfBlack, 2);
+    button = create_rectangle_shape(SUBMIT_BUTTON_SIZE_POS_X, SUBMIT_BUTTON_SIZE_POS_Y, SUBMIT_BUTTON_POS_X, SUBMIT_BUTTON_POS_Y, sfGreen, sfBlack, 2);
+    input_box = create_rectangle_shape(INPUT_BOX_SIZE_POS_X, INPUT_BOX_SIZE_POS_Y, INPUT_BOX_POS_X, INPUT_BOX_POS_Y, sfTransparent, sfBlack, 2);
 
     sfRenderWindow_drawRectangleShape(window, button, NULL);
 
